2b02fd2ef929f9ef09c699a1d50a7b9d36733dcf
Matt Osborn <mattjosborn@gmail.com>
2023-10-24 16:32:06 +0100

    add sticky slot option to SplitScroller

--- src/lib/SplitScrollerSection.svelte
+++ src/lib/SplitScrollerSection.svelte
@@ -11,17 +11,38 @@
 	<div class="scrollable-content">
 		<slot name="scrollable" />
 	</div>
-	{#if sectionEl && $activeSectionEl === sectionEl}
+	{#if $$slots.fixed && sectionEl && $activeSectionEl === sectionEl}
 		<div class="fixed-content">
 			<slot name="fixed" />
+		</div>
+	{:else if $$slots.sticky}
+		<div class="sticky-container">
+			<div class="sticky-content">
+				<slot name="sticky" />
+			</div>
 		</div>
 	{/if}
 </section>
 
 <style>
+	section {
+		position: relative;
+	}
 	.scrollable-content {
 		position: relative;
 		width: 50%;
+	}
+	.sticky-content {
+		position: sticky;
+		top: 0;
+		height: 100vh;
+	}
+	.sticky-container {
+		position: absolute;
+		top: 0;
+		right: 0;
+		width: 50%;
+		height: 100%;
 	}
 	.fixed-content {
 		display: flex;
